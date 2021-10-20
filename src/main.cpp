8e121cb2626b8d00a5682a78780271f310a5f299
Mai Huy Hoang <maihuyhoang97@gmail.com>
2021-10-20 17:01:29 +0700

    feat: add tests

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,24 @@ bool verbose = false;
 bool debug_mode = false;
 
 void print_usage() {
-  printf("Usage: ./riscv-simulator [-v] <elf-path>\n");
+  printf("Usage: ./riscv-simulator [options] <elf-path>\n");
   printf("Options: \n");
-  printf("  -v    Verbose mode\n");
-  printf("  -d    Debug mode\n");
+  printf("  -h              Print this message\n");
+  printf("  -v              Verbose mode\n");
+  printf("  -d              Debug mode\n");
+  printf("Debug: \n");
+  printf("  r               Run till end\n");
+  printf("  n               Next cycle\n");
+  printf("  c               Run till breakpoint\n");
+  printf("  b               Set current instruction as breakpoint\n");
+  printf("  bi <address>    Set instruction at <address> as breakpoint\n");
+  printf("  bs <symbol>     Set symbol as breakpoint\n");
+  printf("  wr a            Watch all register values\n");
+  printf("  wr <reg>        Watch register <reg> value\n");
+  printf("  wmb <address>   Watch 1 byte memory (byte) at <address>\n");
+  printf("  wmh <address>   Watch 2 bytes memory (half) at <address>\n");
+  printf("  wmw <address>   Watch 4 bytes memory (word) at <address>\n");
+  printf("  wmd <address>   Watch 8 bytes memory (double) at <address>\n");
 }
 
 int main(int argc, char *argv[]) {
